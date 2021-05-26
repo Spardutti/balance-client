c6cbe26626737e12bf8c9636025c0ff9d22eea22
Spardutti <luisdamian.sp@gmail.com>
2021-05-26 18:55:33 -0300

    working on displaying data filtered

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import AddItem from "./AddItem";
+import uniqid from "uniqid";
 
 const Table = (props) => {
   const [userItems, setUserItems] = useState();
+  const [itemYear, setItemYear] = useState([]);
+  const [itemMonth, setItemMonth] = useState([]);
 
   //GET ALL YEAR OF ITEMS
   const getYears = () => {
     userItems.map((item) => {
-      console.log(item);
+      if (itemYear.indexOf(item.year) === -1) {
+        setItemYear([...itemYear, item.year]);
+      }
     });
+  };
+
+  const getMonths = () => {
+    let itemArr = [];
+    userItems.map((item) => {
+      // check if the month year exist in the array
+      let monthYear = { year: item.year, month: item.month };
+      if (itemArr.length === 0) {
+        itemArr.push(monthYear);
+      }
+      console.log(itemArr.indexOf(monthYear));
+    });
+    console.log(itemArr);
   };
 
   //GET USER ITEMS ON LOAD/MOUNT
@@ -31,12 +49,43 @@ const Table = (props) => {
   useEffect(() => {
     if (userItems) {
       getYears();
+      getMonths();
     }
   }, [userItems]);
 
   return (
     <div>
       <AddItem userInfo={props.userInfo} token={props.token} />
+      {/* SEARCH FOR THE YEAR ITEMS TO DISPLAY IT ONLY ONCE */}
+      {itemYear.map((year) => {
+        return (
+          <div key={uniqid()}>
+            <h1>{year}</h1>
+            {/* FIND THE ITEMS MATCHING THE CURRENT YEAR AND MONTH*/}
+            {itemMonth.map((month) => {
+              if (month.year === year) {
+                return (
+                  <div key={uniqid()}>
+                    <h5>{month.month}</h5>
+                    {/* DISPLAY THE ITEMS THAT MATCH THE YEAR AND MONTH*/}
+                    {userItems.map((item) => {
+                      if (item.year === year && item.month === month.month) {
+                        return (
+                          <div key={uniqid()}>
+                            <p>
+                              {item.name} & {item.price}
+                            </p>
+                          </div>
+                        );
+                      }
+                    })}
+                  </div>
+                );
+              }
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 };
