37ab2256a35bf1e91d311d6473428280703a2081
Spardutti <luisdamian.sp@gmail.com>
2021-06-07 17:30:55 -0300

    welcome msg

--- src/Components/FirstVisit.jsx
+++ src/Components/FirstVisit.jsx
@@ -8,17 +8,28 @@ const FolderModal = (props) => {
 
   const acceptButton = () => {
     toggle();
+    console.log("clicked");
   };
 
   return (
     <Modal isOpen={props.firstVisit}>
-      <ModalHeader toggle={toggle}>Create Folder</ModalHeader>
+      <ModalHeader toggle={toggle}>Welcome!</ModalHeader>
       <ModalBody>
-        <p>Hello and Welcome</p>
+        <p>
+          Items you add will be saved in the current month and year, you can
+          specify a folder to keep things organized, when a new month starts,
+          the program will automatically save new items in the new month. <br />{" "}
+          Same goes for the year. <br />
+          you cand edit and delete items description and price, but not the
+          date, be careful with that. <br />
+          In order to delete a folder you must first delete all the items
+          belonging to that specific folder. <br />
+          Hope you find this app usefull and thanks for the visit!
+        </p>
       </ModalBody>
       <ModalFooter>
         <Button onClick={acceptButton} className="bg-primary">
-          Okay!
+          Got it!
         </Button>
       </ModalFooter>
     </Modal>
